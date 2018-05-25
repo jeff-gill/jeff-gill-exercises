0792ebd4aee2752c0ac9977296d2caffb13d1db6
jeff-gill <jeffrey.gill@outlook.com>
2018-05-25 17:00:41 -0400

    2/3

--- m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/LinearConvert.java
+++ m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/LinearConvert.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  The foot to meter conversion formula is:
  	m = f * 0.3048
@@ -19,7 +21,33 @@ package com.techelevator;
 public class LinearConvert {
 
 	public static void main(String[] args) {
-
+		Scanner in = new Scanner (System.in);
+		
+		System.out.print("Please neter a length: ");
+		
+		String lengthEntry = in.nextLine();
+		
+		System.out.print("Is the measurement in (m)eter or (f)? ");
+				
+		String choice = in.nextLine().toLowerCase();
+		
+		double amount = Double.parseDouble(lengthEntry);
+		
+		String outcome = "";
+		
+		
+		switch(choice) {
+			case "f":
+				double mTotal = (amount * .3048);
+				outcome = (lengthEntry + "f is %1.4f" + "m");
+				System.out.printf(outcome,  mTotal);
+				break;
+			case "m":
+				double fTotal = (amount * 3.2808399);
+				outcome = (lengthEntry + "m is %1.4f" + "f");
+				System.out.printf(outcome, fTotal);
+				break;
 	}
-
+	}
 }
+
--- m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/TempConvert.java
+++ m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/TempConvert.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  The Fahrenheit to Celsius conversion formula is:
  	Tc = (Tf - 32) / 1.8
@@ -22,7 +24,35 @@ package com.techelevator;
 public class TempConvert {
 
 	public static void main(String[] args) {
-
+		Scanner in = new Scanner (System.in);
+		
+		System.out.print("Please enter a temperature: ");
+		
+		String tempEntry = in.nextLine();
+		
+		System.out.print("Is the temperature (C)elcius, or (F)arenheit? ");
+		
+		String choice = in.nextLine().toUpperCase();
+	
+		double amount = Double.parseDouble(tempEntry);
+		
+		String outcome = "";
+		
+		
+		switch(choice) {
+			case "F":
+				double cTotal = ((amount - 32) / 1.8);
+				outcome = (tempEntry + "F is %1.1f" + "C");
+				System.out.printf(outcome, cTotal);
+				break;
+	
+			case "C":
+				double fTotal = ((amount * 1.8) + 32);
+				outcome = (tempEntry + "C is %1.1f" + "F ");
+			System.out.printf(outcome, fTotal);
+			break;
+		}
+	
 	}
-
 }
+		
