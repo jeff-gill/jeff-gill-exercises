cbda6e7ab15889a8f762c564f90beff241e291a6
jeff-gill <jeffrey.gill@outlook.com>
2018-06-12 21:45:54 -0400

    part one ROman Numerals done

--- m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataRomanNumerals.java
+++ m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataRomanNumerals.java
@@ -1,49 +1,55 @@
 package com.techelevator;
 
+import java.util.TreeMap;
+
 public class KataRomanNumerals {
-	
-	// private final static int 
 
-	public String romanNumerals(int value) {
+		public TreeMap<Integer, String> arabicToRoman = new TreeMap<Integer, String>(); {
 
-		String returnValue = "";
+			arabicToRoman.put(1000, "M");
+			arabicToRoman.put(900, "CM");
+			arabicToRoman.put(500, "D");
+			arabicToRoman.put(100, "C");
+			arabicToRoman.put(50, "L");
+			arabicToRoman.put(10, "X");
+			arabicToRoman.put(5, "V");
+			arabicToRoman.put(1, "I");
+			arabicToRoman.put(0, "");
+	    }
 
-		if (value == 0) {
-			return returnValue;
-		}
-		if (value == 1) {
-			returnValue = "I";
-			return returnValue;
-		}
-		if (value == 5) {
-			returnValue = "V";
-			return returnValue;
-		}
-		if (value == 10) {
-			returnValue = "X";
-			return returnValue;
-		}
-		if (value == 50) {
-			returnValue = "L";
-			return returnValue;
-		}
-		if (value == 100) {
-			returnValue = "C";
-			return returnValue;
-		}
-		if (value == 500) {
-			returnValue = "D";
-			return returnValue;
-		}
-		if (value == 1000) {
-			returnValue = "M";
-			return returnValue;
-		}
-//		if (returnValue.length() < 1) {
-//			return String.valueOf(value);
-//		} 
-		else {
-			return returnValue;
-		}
-	}
+	    public  String toRoman(int arabicNumber) {
+	        int roman =  arabicToRoman.floorKey(arabicNumber);
+	        if ( arabicNumber == roman ) {
+	            return arabicToRoman.get(arabicNumber);
+	        }
+	        return arabicToRoman.get(roman) + toRoman(arabicNumber - roman);
+	    }
 }
+
+    
+//	    public TreeMap<String, Integer> romanToArabic = new TreeMap<String, Integer>(); {
+//
+//	    	romanToArabic.put("M", 1000);
+//	    	romanToArabic.put("CM", 900);
+//	    	romanToArabic.put("D", 500);
+//	    	romanToArabic.put("C", 100);
+//	    	romanToArabic.put("L", 50);
+//	    	romanToArabic.put("X", 10);
+//	    	romanToArabic.put("V", 5);
+//	        romanToArabic.put("I", 1);
+//	        romanToArabic.put("", 0);
+//	    }
+//
+//	    public  Integer toArabic(String i) {
+//	        String arabic =  romanToArabic.floorKey(i);
+//	        if ( i == arabic ) {
+//	            return romanToArabic.get(i);
+//	        }
+//		return romanToArabic.get(arabic) + (toArabic(Integer.parseInt(i)) - (Integer.parseInt(arabic)));
+//	    }
+//
+//		public String toArabic(int parseInt) {
+//			// TODO Auto-generated method stub
+//			return 0;
+//		}
+        
--- m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataRomanNumeralsTest.java
+++ m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataRomanNumeralsTest.java
@@ -6,55 +6,57 @@ import org.junit.Test;
 
 public class KataRomanNumeralsTest {
 	
-	KataRomanNumerals romanNumerals;
+	KataRomanNumerals toRoman;
+	KataRomanNumerals toArabic;
+	
 	
 	@Before
-	public void setup () {
-		romanNumerals = new KataRomanNumerals();
-}
+	public void setup1 () {
+		toRoman = new KataRomanNumerals();
+	}		
 	
 	@Test
 	public void return_empty_string_if_number_is_zero() {
-		Assert.assertEquals("", romanNumerals.romanNumerals(0));
+		Assert.assertEquals("", toRoman.toRoman(0));
 	}
 	
 	@Test
 	public void return_i_if_the_numer_is_1() {
-		Assert.assertEquals("I", romanNumerals.romanNumerals(1));
+		Assert.assertEquals("I", toRoman.toRoman(1));
 	}
 
 	@Test
 	public void return_v_if_the_number_is_5() {
-		Assert.assertEquals("V", romanNumerals.romanNumerals(5));
+		Assert.assertEquals("V", toRoman.toRoman(5));
 	}
 	
 	@Test
 	public void return_x_if_the_number_is_10() {
-		Assert.assertEquals("X", romanNumerals.romanNumerals(10));
+		Assert.assertEquals("X", toRoman.toRoman(10));
 	}
 
 	@Test
 	public void return_L_if_the_number_is_50() {
-		Assert.assertEquals("L", romanNumerals.romanNumerals(50));
+		Assert.assertEquals("L", toRoman.toRoman(50));
 	}
 	
 	@Test
 	public void return_c_if_the_number_is_100() {
-		Assert.assertEquals("C", romanNumerals.romanNumerals(100));
+		Assert.assertEquals("C", toRoman.toRoman(100));
 	}
 	
 	@Test
 	public void return_d_if_the_number_is_500() {
-		Assert.assertEquals("D", romanNumerals.romanNumerals(500));
+		Assert.assertEquals("D", toRoman.toRoman(500));
 	}
 	
 	@Test
 	public void return_m_if_the_number_is_1000() {
-		Assert.assertEquals("M", romanNumerals.romanNumerals(1000));
+		Assert.assertEquals("M", toRoman.toRoman(1000));
 	}
 	
 	@Test
-	public void return_mi_if_the_number_if_1001() {
-		Assert.assertEquals("MI", romanNumerals.romanNumerals(1001));
+	public void return_mi_if_the_number_is_1001() {
+		Assert.assertEquals("MI", toRoman.toRoman(1001));
 	}
 }
