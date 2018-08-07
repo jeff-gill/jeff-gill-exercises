5fa7ffff7f191ed53979b466b52972ad2dbecbd2
jeff-gill <jeffrey.gill@outlook.com>
2018-08-07 11:56:37 -0400

    merge conflict fixed

--- m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
+++ m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -9,42 +9,6 @@ public class KataFizzBuzz {
 
 	private final static int FIZZABLE = 3;
 	private final static int BUZZABLE = 5;
-<<<<<<< HEAD
-	private final static int RANGEFLOOR = 1;
-	private final static int RANGECEILING = 100;
-	private final static String FIZZ = "Fizz";
-	private final static String BUZZ = "Buzz";
-
-	public String fizzBuzz(int value) {
-
-		String returnValue = "";
-
-		if (value < RANGEFLOOR || value > RANGECEILING) {
-			return returnValue;
-		}
-
-		if (isDivisibleBy(FIZZABLE, value) || containsDigit(FIZZABLE, value)) {
-			returnValue += FIZZ;
-		}
-		if (isDivisibleBy(BUZZABLE, value) || containsDigit(BUZZABLE, value)) {
-			returnValue += BUZZ;
-		}
-
-		if (returnValue.length() < 1) {
-			return String.valueOf(value);
-		} else {
-			return returnValue;
-		}
-	}
-
-	public boolean isDivisibleBy(int multiple, int number) {
-		return number % multiple == 0;
-	}
-
-	private boolean containsDigit(int digit, int number) {
-		return String.valueOf(number).contains(String.valueOf(digit));
-	}
-=======
 	private final static int RANGE_FLOOR = 1;
 	private final static int RANGE_CEILING = 100;
 	private final static String FIZZ = "Fizz";
@@ -82,6 +46,5 @@ public class KataFizzBuzz {
 	private boolean containsDigit(int digit, int number) {
 		return String.valueOf(number).contains(String.valueOf(digit));
 	}
-	
->>>>>>> 2e50540c635802e7c7aafa33c8d8d71a918e277c
+
 }
--- m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -6,120 +6,74 @@ import static org.junit.Assert.*;
 public class KataFizzBuzzTest {
 
 	KataFizzBuzz fizzBuzz;
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 2e50540c635802e7c7aafa33c8d8d71a918e277c
 	@Before
 	public void setup() {
 		fizzBuzz = new KataFizzBuzz();
 	}
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 2e50540c635802e7c7aafa33c8d8d71a918e277c
 	@Test
 	public void return_empty_string_for_numbers_out_of_range() {
 		Assert.assertEquals("", fizzBuzz.fizzBuzz(0));
 		Assert.assertEquals("", fizzBuzz.fizzBuzz(101));
 	}
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 2e50540c635802e7c7aafa33c8d8d71a918e277c
 	@Test
 	public void return_number_if_not_divisible_or_contains_3_or_5() {
 		Assert.assertEquals("1", fizzBuzz.fizzBuzz(1));
 		Assert.assertEquals("44", fizzBuzz.fizzBuzz(44));
 		Assert.assertEquals("98", fizzBuzz.fizzBuzz(98));
 	}
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 2e50540c635802e7c7aafa33c8d8d71a918e277c
 	@Test
 	public void return_fizz_for_multiples_of_3() {
 		Assert.assertEquals("Fizz", fizzBuzz.fizzBuzz(6));
 		Assert.assertEquals("Fizz", fizzBuzz.fizzBuzz(42));
 		Assert.assertEquals("Fizz", fizzBuzz.fizzBuzz(99));
 	}
-<<<<<<< HEAD
 
-	@Test
-	public void return_fizz_for_multiples_of_5() {
-=======
 	
 	@Test
 	public void return_buzz_for_multiples_of_5() {
->>>>>>> 2e50540c635802e7c7aafa33c8d8d71a918e277c
+
 		Assert.assertEquals("Buzz", fizzBuzz.fizzBuzz(10));
 		Assert.assertEquals("Buzz", fizzBuzz.fizzBuzz(70));
 		Assert.assertEquals("Buzz", fizzBuzz.fizzBuzz(100));
 	}
-<<<<<<< HEAD
 
-	@Test
-	public void return_fizz_for_multiples_of_3_and_5() {
-=======
 	
 	@Test
 	public void return_fizzbuzz_for_multiples_of_3_and_5() {
->>>>>>> 2e50540c635802e7c7aafa33c8d8d71a918e277c
+
 		Assert.assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(15));
 		Assert.assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(60));
 		Assert.assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(90));
 	}
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 2e50540c635802e7c7aafa33c8d8d71a918e277c
 	@Test
 	public void return_fizz_if_contains_3() {
 		Assert.assertEquals("Fizz", fizzBuzz.fizzBuzz(13));
 		Assert.assertEquals("Fizz", fizzBuzz.fizzBuzz(43));
-<<<<<<< HEAD
-		Assert.assertEquals("Fizz", fizzBuzz.fizzBuzz(43));
-	}
 
-=======
 		Assert.assertEquals("Fizz", fizzBuzz.fizzBuzz(83));
 	}
-	
->>>>>>> 2e50540c635802e7c7aafa33c8d8d71a918e277c
+
 	@Test
 	public void return_fizz_if_contains_5() {
 		Assert.assertEquals("Buzz", fizzBuzz.fizzBuzz(52));
 		Assert.assertEquals("Buzz", fizzBuzz.fizzBuzz(58));
 	}
-<<<<<<< HEAD
 
-	@Test
-=======
 	
 	@Test 
->>>>>>> 2e50540c635802e7c7aafa33c8d8d71a918e277c
 	public void return_fizzbuzz_if_multiple_of_3_and_contains_5() {
 		Assert.assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(51));
 		Assert.assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(54));
 	}
-<<<<<<< HEAD
 
-	@Test
-	public void return_fizzbuzz_if_multiple_of_5_and_contains_3() {
-		Assert.assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(35));
-
-	}
-}
-=======
 	
 	@Test 
 	public void return_fizzbuzz_if_multiple_of_5_and_contains_3() {
 		Assert.assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(35));
 	}
 }
->>>>>>> 2e50540c635802e7c7aafa33c8d8d71a918e277c
